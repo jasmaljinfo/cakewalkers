4ed37ffbef0f4f1231049a19f4505e3e6ff1ffed
Ajmal Aju <dev@ajmalaju.com>
2019-06-28 21:19:10 +0530

    fix-clean

---
+++ assets/js/default_dynamic.js
@@ -0,0 +1,20 @@
+
+var header_height = 100;
+var min_header_height_scroll = 57;
+var min_header_height_fixed_hidden = 50;
+var min_header_height_sticky = 60;
+var scroll_amount_for_sticky = 85;
+var content_line_height = 60;
+var header_bottom_border_weight = 1;
+var scroll_amount_for_fixed_hiding = 200;
+var paspartu_width_init = 0.02;
+var add_for_admin_bar = 0;
+	header_height = 100;
+	min_header_height_sticky = 100;
+
+var logo_height = 130; // proya logo height
+var logo_width = 280; // proya logo width
+	logo_height = 50;
+	logo_width = 157;
+
+	header_top_height = 0;
